2d7de4437d643efd3534cb06468bf0299a839146
Dmitry Sorokin <redballtoy@gmail.com>
2021-04-30 13:18:37 +0300

    Add toolbar

--- app/src/main/java/com/example/redballtoy/telegrammclone/MainActivity.kt
+++ app/src/main/java/com/example/redballtoy/telegrammclone/MainActivity.kt
@@ -1,15 +1,42 @@
 package com.example.redballtoy.telegrammclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.redballtoy.telegrammclone.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.util.DrawerNavigationUI
+import com.mikepenz.materialdrawer.widget.AccountHeaderView
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var bindingMain: ActivityMainBinding
+    private lateinit var draverNavigation: DrawerNavigationUI
+    private lateinit var header: AccountHeaderView
+    private lateinit var toolbar: Toolbar
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingMain.root)
     }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunc()
+    }
+
+    //инициализация функциональности
+    private fun initFunc() {
+        setSupportActionBar(toolbar)
+    }
+
+    //инициализация объектов
+    private fun initFields() {
+        toolbar=bindingMain.tbMain
+    }
+
+
 }
